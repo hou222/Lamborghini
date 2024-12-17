883469bec0746e62f006a6bd3e46dd25a017ad64
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-12-17 23:47:10 +0100

    add nav button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 function App() {
   return (
     <div>
-      <header className="bg-black flex justify-between p-4"></header>
+      <header className="bg-black flex justify-between p-4">
+        <div className="w-8 h-8 bg-white"></div>
+        {/* <img src="" alt="" /> */}
+        <div className="flex gap-5">
+          <div className="w-8 h-8 bg-white"></div>
+          <div className="w-8 h-8 bg-white"></div>
+          <div className="w-8 h-8 bg-white"></div>
+          {/* <img src="" alt="" />
+          <img src="" alt="" />
+          <button></button> */}
+        </div>
+      </header>
     </div>
   );
 }
