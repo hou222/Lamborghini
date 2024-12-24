85f246f848f199381424f0c9176e5560762852b0
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-12-24 15:58:41 +0100

    add header icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import Lambo from "./assets/Lamborghini.png";
+import Message from "./assets/Message.png";
+import Search from "./assets/Search.png";
+
 function App() {
   return (
     <div>
-      <header className="bg-black flex justify-between p-4">
-        <div className="w-8 h-8 bg-white"></div>
-        {/* <img src="" alt="" /> */}
-        <div className="flex gap-5">
-          <div className="w-8 h-8 bg-white"></div>
-          <div className="w-8 h-8 bg-white"></div>
-          <div className="w-8 h-8 bg-white"></div>
-          {/* <img src="" alt="" />
-          <img src="" alt="" />
-          <button></button> */}
+      <header className="bg-black flex justify-between px-4 p-2">
+        {/* <div className="w-8 h-8 bg-white"></div> */}
+        <img src={Lambo} alt="Lamborghini Icon" className="w-11 h-11" />
+        <div className="flex gap-7 items-center">
+          <img src={Message} alt="Message Icon" className="w-8 h-8" />
+          <img src={Search} alt="Search Icon" className="w-6 h-6" />
+
+          <button className=" ">
+            <div className="flex flex-col  gap-1.5 w-8 h-8 justify-center p-0.5 lg:hidden">
+              <div className=" bg-white w-full h-[0.05rem]"></div>
+              <div className=" bg-white w-full h-[0.05rem]"></div>
+              <div className=" bg-white w-full h-[0.05rem]"></div>
+            </div>
+          </button>
         </div>
       </header>
     </div>
