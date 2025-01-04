545d41fb7604d904545fd19916e04c2bbffec79b
Houssam Benlagha <houssambenlagha0@gmail.com>
2025-01-04 10:19:27 +0100

    add logos to right list

--- src/scenes/header/index.tsx
+++ src/scenes/header/index.tsx
@@ -5,19 +5,38 @@ import Search from "../../assets/Search.png";
 import Hexa from "../../assets/hexagon.png";
 import UpWhite from "../../assets/UpWhite.png";
 import UpBlack from "../../assets/UpBlack.png";
+import Instagram from "../../assets/instagram.png";
+import Threads from "../../assets/threads.png";
+import Facebook from "../../assets/facebook.png";
+import Youtube from "../../assets/youtube.png";
+import Twitter from "../../assets/twitter.png";
+import Tiktok from "../../assets/tiktok.png";
+import Linkedin from "../../assets/linkedin.png";
+import Weibo from "../../assets/weibo.png";
+import Youku from "../../assets/youku.png";
+import Wechat from "../../assets/wechat.png";
+import Discord from "../../assets/discord.png";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpen1, setIsOpen1] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
 
   function handleOpen() {
-    setIsOpen(!isOpen);
+    setIsOpen((isOpen) => !isOpen);
   }
   function handleOpen1() {
-    setIsOpen1(!isOpen1);
+    if (isOpen2) {
+      setIsOpen2((isOpen2) => !isOpen2);
+    }
+
+    setIsOpen1((isOpen1) => !isOpen1);
   }
   function handleOpen2() {
-    setIsOpen2(!isOpen2);
+    if (isOpen1) {
+      setIsOpen1((isOpen1) => !isOpen1);
+    }
+
+    setIsOpen2((isOpen2) => !isOpen2);
   }
   return (
     <header>
@@ -169,17 +188,50 @@ export default function Header() {
               isOpen2 ? "-top-[480px] border-b" : "top-0"
             } ${!isOpen && " "}`}
           >
-            <li>INSTAGRAM</li>
-            <li>THREADS</li>
-            <li>FACEBOOK</li>
-            <li>YOUTUBE</li>
-            <li>TWITTER</li>
-            <li>TIKTOK</li>
-            <li>LINKEDIN</li>
-            <li>WEIBO</li>
-            <li>YOUKU</li>
-            <li>WECHAT</li>
-            <li>DISCORD</li>
+            <li className="flex gap-3  items-center ">
+              <img src={Instagram} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">INSTAGRAM</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Threads} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">THREADS</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Facebook} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">FACEBOOK</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Youtube} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">YOUTUBE</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Twitter} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">TWITTER</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Tiktok} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">TIKTOK</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Linkedin} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">LINKEDIN</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Weibo} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">WEIBO</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Youku} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">YOUKU</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Wechat} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">WECHAT</p>
+            </li>
+            <li className="flex gap-3 items-center ">
+              <img src={Discord} alt="" className="w-[18px] h-[18px] " />
+              <p className=" text-end">DISCORD</p>
+            </li>
           </ul>
         </div>
       </nav>
