4b3e04af95f6df125ae1ec855e601df6c4b23a75
Houssam Benlagha <houssambenlagha0@gmail.com>
2025-03-20 01:28:30 +0100

    add footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <div className="bg-black text-white flex py-5 justify-between pt-20">
-      <p>dfdfdfffffffffffffff</p>
-      <p>dfdfdfffffffffffffff</p>
-      <p>dfdfdfffffffffffffff</p>
+    <div className="bg-black text-white flex pt-14 pb-7 xl:pt-16 xl:pb-10  justify-center items-center ">
+      <p className="text-center  xl:text-lg">
+        Copyright © 2025 Automobili Lamborghini
+      </p>
     </div>
   );
 }
